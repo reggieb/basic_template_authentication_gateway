fac17aebfc4b4bff5dd113dbfe94c637e78db4d6
Rob Nichols <git@nicholshayes.co.uk>
2013-10-04 09:25:39 +0100

    Declutter: Moves authorisation steps into one controller (authorize) and remove unnecessary files

--- app/controllers/authorize_controller.rb
+++ app/controllers/authorize_controller.rb
@@ -1,6 +1,6 @@
 class AuthorizeController < ApplicationController
 
-  def new
+  def request_from_client_app
     client_app = ClientApp.find_by_client_id(params[:client_id])
     if client_app
       @manifest = client_app.manifests.create(
@@ -14,8 +14,31 @@ class AuthorizeController < ApplicationController
     end
   end
 
+  def response_from_authority
 
-  def create
+     # If the client_id is passed into state like this:
+     #   http://localhost:3000/auth/google_oauth2?state=<client_id>
+     # the client id is returned when the user returns from google.
+     # The state is created by the client app, and this is used
+     # to trace the transaction through from the start.
+    manifest = Manifest.find_by_state(params[:state])
+    client_app = manifest.client_app
+    Person.create(
+      email: user_hash['email'],
+      first_name: user_hash['given_name'],
+      last_name: user_hash['family_name'],
+      manifest: manifest
+    )
+
+    if client_app
+      flash[:notice] = "state was #{params[:state]}"
+      redirect_to "#{client_app.return_url}?code=#{manifest.code}&response_type=code&state=#{params[:state]}"
+    end
+
+  end
+
+
+  def callback_from_client_app
     unless @client_app = ClientApp.authenticate(
                           id: params[:client_id],
                           secret: params[:client_secret]
@@ -24,7 +47,7 @@ class AuthorizeController < ApplicationController
       return
     end
 
-    unless manifest = get_manifest
+    unless manifest = get_manifest_by_code_or_grant_type
       render json: {error: "Could not authenticate access code"}
       return
     end
@@ -36,8 +59,32 @@ class AuthorizeController < ApplicationController
     }
   end
 
+  def user_lookup_by_client_app
+    manifest = Manifest.find_by_access_token(params[:oauth_token])
+    person = manifest.person
+    hash = {
+      provider: 'antechamber',
+      id: person.id.to_s,
+      email: person.email,
+      extra: {
+         first_name: person.first_name,
+         last_name: person.last_name
+      }
+    }
+
+    render json: hash.to_json
+  end
+
   private
-  def get_manifest
+  def omniauth
+    @omniauth ||= request.env['omniauth.auth']
+  end
+
+  def user_hash
+    @user_hash ||= (omniauth && omniauth.extra) ? omniauth.extra.raw_info : {}
+  end
+
+  def get_manifest_by_code_or_grant_type
     if params[:code]
       @client_app.manifests.find_by_code(params[:code])
     else
@@ -45,7 +92,6 @@ class AuthorizeController < ApplicationController
       @client_app.manifests.send("find_by_#{grant_type}", params[grant_type])
     end
   end
-
 
   def forward_to_trusted_authenticator
     redirect_to "/auth/google_oauth2/?state=#{@manifest.state}"
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Antechamber::Application.routes.draw do
 
   devise_for :users
 
-  get "auth/authorize", to: 'authorize#new', as: 'request_from_client_app'
+  get "auth/authorize", to: 'authorize#request_from_client_app'
 
-  get "auth/:id/callback", to: 'callback#show', as: 'response_from_authority'
+  get "auth/:id/callback", to: 'authorize#response_from_authority'
 
-  post "oauth/token", to: 'authorize#create', as: 'callback_from_client_app'
+  post "oauth/token", to: 'authorize#callback_from_client_app'
 
-  get "auth/user", to: 'people#show', as: 'user_lookup_by_client_app'
+  get "auth/user", to: 'authorize#user_lookup_by_client_app'
 
   root :to => "users#index"
 
